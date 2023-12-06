3f74500ecdb8217d0afb1448080e0be91e645d47
AstridM15 <astridmucha6@gmail.com>
2023-12-06 16:31:16 -0500

    vfgfdg

--- api/muestra.js
+++ api/muestra.js
@@ -2,10 +2,8 @@ const express = require('express');
 const ruta = express.Router();
 const db = require('../db/models/index');
 const { reserva, libro, usuario } = require('../db/models'); 
-// Ruta para obtener libros con búsqueda
 ruta.get('/libros', async (req, res) => {
     try {
-      // Cambia 'TrabajoFinalProgra' por el nombre de tu base de datos
       const libros = await db.libro.findAll({
         attributes: ['id', 'titulo', 'autor', 'descripcion', 'editorial', 'ISBN', 'categoria', 'imagenLibro', 'cantidadEjemplares'],
       });
@@ -25,14 +23,12 @@ ruta.get('/libros', async (req, res) => {
     const { idUsuario, idLibro, fechainicio, fechafinal } = req.body;
   
     try {
-      // Verificar si el libro existe
       const libroDisponible = await libro.findByPk(idLibro);
   
       if (!libroDisponible) {
         return res.status(400).json({ mensaje: 'El libro no existe o no está disponible para reserva.' });
       }
   
-      // Crear la reserva
       const nuevaReserva = await reserva.create({
         idUsuario,
         idLibro,
@@ -48,26 +44,122 @@ ruta.get('/libros', async (req, res) => {
   });
   
 
-  ruta.get('/ultimas', async (req, res) => {
-    try {
-        // Cambia 'TrabajoFinalProgra' por el nombre de tu base de datos
-        const reservas = await db.reserva.findAll({
-          attributes: ["id","fechainicio","fechafinal","idUsuario","idLibro"],
-        });
-
-        const libroDisponible = await libro.findAll(idLibro);
-        
-    
-        if (reservas.length === 0) {
-          return res.status(404).json({ error: 'No se encontraron libros' });
-        }
-    
-        res.json({ reservas });
-      } catch (error) {
-        console.error('Error al obtener los libros:', error);
-        res.status(500).json({ error: 'Error interno del servidor' });
-      }
-    });
+ ruta.get('/reservasPorUsuario/:idUsuario', async (req, res) => {
+    const idUsuario = req.params.idUsuario;
   
+    try {
+      const reservas = await db.reserva.findAll({
+        where: { idUsuario: idUsuario },
+        include: [{ model: db.libro, as: 'libroreserva', attributes: ['titulo', 'imagenLibro'] }],
+      });
+  
+      if (reservas.length === 0) {
+        return res.status(404).json({ error: 'No se encontraron reservas para este usuario' });
+      }
+  
+      const resultados = reservas.map((reserva) => ({
+        libro: {
+          titulo: reserva.libroreserva.titulo,
+          imagenLibro: reserva.libroreserva.imagenLibro,
+        },
+        reserva: {
+          fechaInicio: reserva.fechainicio,
+          fechaFinal: reserva.fechafinal,
+        },
+      }));
+  
+      res.json(resultados);
+    } catch (error) {
+      console.error('Error al obtener las reservas:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  });
+
+ruta.get('/librosMasPedidosPorUsuario/:idUsuario', async (req, res) => {
+    const idUsuario = req.params.idUsuario;
+  
+    try {
+      const reservas = await db.reserva.findAll({
+        where: { idUsuario: idUsuario },
+        include: [
+          {
+            model: db.libro,
+            as: 'libroreserva',
+            attributes: ['id', 'titulo', 'imagenLibro'],
+          },
+        ],
+      });
+  
+      const pedidosPorLibro = {};
+      reservas.forEach((reserva) => {
+        const libroId = reserva.libroreserva.id;
+        if (!pedidosPorLibro[libroId]) {
+          pedidosPorLibro[libroId] = {
+            libro: reserva.libroreserva,
+            cantidadPedidos: 0,
+          };
+        }
+        pedidosPorLibro[libroId].cantidadPedidos += 1;
+      });
+  
+      // Ordenar los libros por la cantidad de pedidos en orden descendente
+      const librosMasPedidos = Object.values(pedidosPorLibro)
+        .sort((a, b) => b.cantidadPedidos - a.cantidadPedidos)
+        .slice(0, 10);
+  
+      res.json(librosMasPedidos);
+    } catch (error) {
+      console.error('Error al obtener los libros más pedidos:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  });
+
+  function sumarDiasAFecha(fecha, dias) {
+    const nuevaFecha = new Date(fecha);
+    nuevaFecha.setDate(nuevaFecha.getDate() + dias);
+    return nuevaFecha;
+  }
+  
+
+ ruta.get('/librosProximosAVencer/:idUsuario', async (req, res) => {
+    const idUsuario = req.params.idUsuario;
+  
+    try {
+      const reservas = await db.reserva.findAll({
+        where: { idUsuario: idUsuario },
+        include: [
+          {
+            model: db.libro,
+            as: 'libroreserva',
+            attributes: ['id', 'titulo', 'imagenLibro'],
+          },
+        ],
+      });
+  
+      const hoy = new Date(); 
+      const librosAMostrar2 = reservas.map((reserva) => {
+        const fechaFinalReserva = new Date(reserva.fechafinal);
+          const diasRestantes = Math.floor((fechaFinalReserva - hoy) / (1000 * 60 * 60 * 24));
+  
+        if (diasRestantes < 20) {
+          return {
+            titulo: reserva.libroreserva.titulo,
+            imagenLibro: reserva.libroreserva.imagenLibro,
+            fechaReserva: reserva.fechainicio,
+            fechaFinal: reserva.fechafinal,
+            imagenPortadaUrl: reserva.libroreserva.imagenLibro,
+            diasRestantes: diasRestantes,
+          };
+        }
+  
+        return null;
+      }).filter(Boolean);
+  
+      res.json(librosAMostrar2);
+    } catch (error) {
+      console.error('Error al obtener las reservas próximas a vencer:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+});
   
   module.exports = ruta;
